a7d86b2bd90bc69129865bc88d266e105b6a8607
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-25 16:48:59 -0600

    Add files via upload

---
+++ pr_archivo_nolasco_0948.py
@@ -0,0 +1,9 @@
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+archivo=open("alumnos.txt","r")#abro mi archivo de texto para solo lectura
+print(archivo.read())#indico que se ingrese la informacion del archvo.nxt
+
+
+archivo.close()#se cierra elarchivo de lectura 
+
+
